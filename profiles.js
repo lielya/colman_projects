8383e3f526d49cf4c1473d5cd418739b3a750b24
Liely <62380402+lielya@users.noreply.github.com>
2025-09-24 21:17:16 +0300

    Update profiles.js
    Update ids and names

--- profiles.js
+++ profiles.js
@@ -1,3 +1,5 @@
+<!--Amit-Mosseri-206446791-Liel-Yaakobov-322366311-Lihi-Skif-322235888-->
+
 document.addEventListener("DOMContentLoaded", () => {
   // Dummy profiles, same order as in profiles.html
   const profiles = [
