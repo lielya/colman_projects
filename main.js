799fdc0309996cf0a8c9b44279f73628c16023fe
Liely <62380402+lielya@users.noreply.github.com>
2025-09-24 21:16:30 +0300

    Update main.js
    Update ids and names

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+<!--Amit-Mosseri-206446791-Liel-Yaakobov-322366311-Lihi-Skif-322235888-->
+
 // Header population and logout
 const nameLS = localStorage.getItem("selectedProfileName");
 const avatarLS = localStorage.getItem("selectedProfileAvatar");
