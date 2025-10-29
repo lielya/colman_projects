6a7eeab06e5c06c28d29813ba740a6c28182deeb
lielya <liel.nana1@gmail.com>
2025-10-29 21:08:43 +0200

    Add controllers files

---
+++ controllers/authController.js
@@ -0,0 +1,34 @@
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+
+exports.registerUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) return res.status(400).json({ message: "User already exists" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ username, email, password: hashedPassword });
+    await user.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "User not found" });
+
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) return res.status(401).json({ message: "Invalid password" });
+
+    res.json({ message: "Login successful", user });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
---
+++ controllers/profileController.js
@@ -0,0 +1,57 @@
+const Profile = require('../models/profiles');
+
+// list all profiles
+exports.getAllProfiles = async (req, res) => {
+  try {
+    const profiles = await Profile.findAll(); 
+    res.json(profiles);
+  } catch (err) {
+    res.status(500).json({ error: 'can not find all profiles' });
+  }
+};
+
+
+exports.getProfileById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const profile = await Profile.findById(id);
+    if (!profile) {
+      return res.status(404).json({ error: 'can not find profile' });
+    }
+    res.json(profile);
+  } catch (err) {
+    res.status(500).json({ error: 'can not get desired profile'});
+  }
+};
+
+// create a new profile
+exports.createProfile = async (req, res) => {
+  try {
+    const newProfile = await Profile.create(req.body);
+    res.status(201).json(newProfile);
+  } catch (err) {
+    res.status(400).json({ error: 'can not create new profile' });
+  }
+};
+
+// update an exist profile
+exports.updateProfile = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updated = await Profile.update(id, req.body);
+    res.json(updated);
+  } catch (err) {
+    res.status(400).json({ error: 'can not update profile' });
+  }
+};
+
+// delete profile
+exports.deleteProfile = async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Profile.delete(id);
+    res.json({ message: 'profile deleted successfully' });
+  } catch (err) {
+    res.status(400).json({ error: 'can not delete profile' });
+  }
+};
