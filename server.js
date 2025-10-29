92a67af248289d9e3d271534b3fb4e313a13280d
lielya <liel.nana1@gmail.com>
2025-10-29 20:16:32 +0200

    Add server configuration

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const dotenv = require("dotenv");
+const app = express();
+
+dotenv.config();
+
+//connect to mongoDB
+mongoose.connect(process.env.MONGO_URI || "mongodb://localhost/netflixDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log("MongoDB connected"))
+  .catch(err => console.log("MongoDB error:", err));
+
+//general 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const indexRoutes = require("./routes/index");
+app.use("/", indexRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
